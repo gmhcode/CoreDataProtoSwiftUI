511cfead43506d4d90e332467f2d540ad07df7a8
Greg <greg@passivelogic.com>
2021-07-09 12:51:49 -0600

    Initial Commit

---
+++ CoreDataProtoSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreDataProtoSwiftUI
+//
+//  Created by Greg Hughes on 7/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CoreDataProtoSwiftUI/CoreDataProtoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProtoSwiftUIApp.swift
+//  CoreDataProtoSwiftUI
+//
+//  Created by Greg Hughes on 7/9/21.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProtoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
